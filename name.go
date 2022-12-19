4b8a336c8af47ec5e19c39063fb4074b4c0fedba
Michael Rybka <merybka@gmail.com>
2022-12-18 22:17:53 -0500

    Add name mutations

--- name.go
+++ name.go
@@ -1,8 +1,37 @@
 package nameconv
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 type Name []string
+
+// AppendRune appends a rune to the end of the last word in the name.
+func (n Name) AppendRune(r rune) {
+	r = unicode.ToLower(r)
+	if len(n) == 0 {
+		n = Name{string(r)}
+	}
+	n[len(n)-1] += string(r)
+}
+
+// AppendWord appends a word to the end of the name.
+func (n Name) AppendWord(s string) {
+	n = append(n, s)
+}
+
+func (n Name) Equal(other Name) bool {
+	if len(n) != len(other) {
+		return false
+	}
+	for i := range n {
+		if n[i] != other[i] {
+			return false
+		}
+	}
+	return true
+}
 
 func (n Name) String() string {
 	return strings.Join(n, " ")
